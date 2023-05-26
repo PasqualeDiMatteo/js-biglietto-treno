cfae71a8b54ca79b3640647c412200765552f437
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-05-26 14:30:30 +0200

    (JS) Added Costants

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 console.log("JSOK");
+
+/*
+1. Costanti :
+- €/km
+- Age Senior
+- Age Minors
+- Discount for Senior
+- Discount for Minors
+2. Chiedere all'utente i km che vuole percorrere
+3. Chiedere all'utente l'età
+4. Validazione
+5. Calcolare il prezzo del bigietto
+6. Applicare lo sconto
+7. Calcolare il prezzo con lo sconto
+8. Stampare il prezzo con lo sconto 
+*/
+
+// 1. Costants
+
+const priceKm = 0.21;
+const ageSenior = 65;
+const ageMinors = 18;
+const discountSenior = 40;
+const discountMinors = 20;
